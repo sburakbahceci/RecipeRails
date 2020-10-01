129add689c47901b45b84908b31a56a24e127bd4
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-01 14:47:24 +0300

    Add Users and finish styles

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,11 +9,11 @@ class RecipesController < ApplicationController
 	end
 
 	def new
-		@recipe = Recipe.new
+		@recipe = current_user.recipes.build
 	end
 
 	def create
-		@recipe = Recipe.new(recipe_params)
+		@recipe = current_user.recipes.build(recipe_params)
 
 		if @recipe.save
 			redirect_to @recipe, notice: "Successfully created new recipe"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_081914) do
+ActiveRecord::Schema.define(version: 2020_10_01_111410) do
 
   create_table "directions", force: :cascade do |t|
     t.text "step"
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 2020_10_01_081914) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "directions", "recipes"
